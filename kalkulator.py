28522d94fc8451fa6a1aaee709b10a67f38657e1
matZdan <fazol66@gmail.com>
2016-04-10 10:33:15 +0200

    zmieniona funkcja dodawania ostatecznie dziala

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,9 @@
 def dodawanie(a, b):
 	print(a+b)
-
-l1 = int(input())
-l2 = int(input())	
-print(dodawanie(l1, l2))
+try:	
+	l1 = int(input())
+	l2 = int(input())	
+	print(dodawanie(l1, l2))
+except:
+	print("program zakonczyl sie nieoczekiwanym bledem")
+	print("mozesz go zglosic pod adresem pomoc@autor.pl")
